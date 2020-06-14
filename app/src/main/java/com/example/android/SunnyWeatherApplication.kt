629ec6b5c6949371684b32b647ca1673969ad25c
Tian <guotian@mail.com>
2020-06-14 23:14:37 +0800

    定义全局Context， 定义模型数据

---
+++ app/src/main/java/com/example/android/SunnyWeatherApplication.kt
@@ -0,0 +1,20 @@
+package com.example.android
+
+import android.annotation.SuppressLint
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication : Application() {
+    companion object {
+        const val TOKEN = ""
+
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
